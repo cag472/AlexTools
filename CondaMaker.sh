85dcbef925b11ff46b306fb75a418cc197853d6d
cag472 <cag472@gmail.com>
2022-10-17 14:52:21 -0400

    Update CondaMaker.sh

--- CondaMaker.sh
+++ CondaMaker.sh
@@ -1,8 +1,18 @@
+
+#Set up the channel and get python
 conda config --add channels conda-forge
 conda update conda
 conda create --name py310 
-conda actiavte py310
+conda activate py310
 conda init bash
 conda install python=3.10
-conda install -c conda-forge pytorch-gpu==1.12.1 # see available versions here: https://anaconda.org/conda-forge/pytorch-gpu/files
 
+#Now for Torch. Make sure this works -- torchvision gives an error if you use 0.13 as of the time I write this.
+#See available versions here: https://anaconda.org/conda-forge/pytorch-gpu/files
+conda install -c conda-forge pytorch-gpu torchvision==0.12 
+
+#General stuff
+conda install pyyaml
+
+#Cartopy and map stuff
+conda install cartopy geons cairo pyyaml haversine netcdf4 
