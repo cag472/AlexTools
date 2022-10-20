53d0def7d267ba8ab32eaa722d54ac03db9b97d8
cag472 <cag472@gmail.com>
2022-10-20 02:24:20 -0400

    Update CondaMaker.sh

--- CondaMaker.sh
+++ CondaMaker.sh
@@ -12,11 +12,17 @@ conda install python=3.10
 conda install -c conda-forge pytorch-gpu torchvision==0.12 
 
 #General stuff
-conda install pyyaml gitpython pandas pip pydeprecate
+conda install pyyaml gitpython pandas pip pydeprecate lxml seaborn
 
 #Cartopy and map stuff
 conda install cartopy geos cairo haversine netcdf4 
 
+#Flask
+conda install -c anaconda flask 
+
 #Other ML stuff
 #This will probably install the CPU version of Tensorflow. But that's OK, I never use Tensorflow anyway. 
-conda install scikit-image scikit-learn keras scipy tensorflow torchaudio tensorboard pytorch-lightning lightning-bolts
+conda install scikit-image scikit-learn keras scipy tensorflow torchaudio tensorboard pytorch-lightning lightning-bolts cleanlab doubtlab 
+
+#Other stuff
+conda install rasterio osgeo 
